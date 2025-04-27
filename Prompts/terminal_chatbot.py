11a8d300e07cacc50d256c325ba47cbb6964cda7
0XSoumya <soumyasahoo.2907@gmail.com>
2025-04-27 11:37:51 +0530

    added more code

--- Prompts/terminal_chatbot.py
+++ Prompts/terminal_chatbot.py
@@ -1,15 +1,22 @@
 from langchain_mistralai import ChatMistralAI
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
 model = ChatMistralAI(model = 'mistral-small')
 
+chat_history=[
+    SystemMessage(comtent='you are a helpful assistant who gives concise answers to the queries'),
+
+]
 
 while True:
     query = input('You :')
-    
+    chat_history.append(HumanMessage(content= 'query'))
     if query == 'exit' :
         break
-    response = model.invoke(query)
+    response = model.invoke(chat_history)
+    chat_history.append(response.content)
     print('Mistral :', response.content)
+print(chat_history)
