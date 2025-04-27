c4a74f4080ca38c9278b467c2245e406203c55a0
0XSoumya <soumyasahoo.2907@gmail.com>
2025-04-27 11:48:53 +0530

    added chat history

--- Prompts/terminal_chatbot.py
+++ Prompts/terminal_chatbot.py
@@ -1,22 +1,23 @@
 from langchain_mistralai import ChatMistralAI
 from dotenv import load_dotenv
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from colorama import init, Fore, Style
 
 load_dotenv()
 
 model = ChatMistralAI(model = 'mistral-small')
 
 chat_history=[
-    SystemMessage(comtent='you are a helpful assistant who gives concise answers to the queries'),
+    SystemMessage(content='you are a helpful assistant who gives concise answers to the queries')
 
 ]
 
 while True:
     query = input('You :')
-    chat_history.append(HumanMessage(content= 'query'))
+    chat_history.append(HumanMessage(content= query))
     if query == 'exit' :
+        
         break
     response = model.invoke(chat_history)
-    chat_history.append(response.content)
+    chat_history.append(AIMessage(content=response.content))
     print('Mistral :', response.content)
-print(chat_history)
