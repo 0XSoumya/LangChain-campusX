2f726c92422a4b8455cb1bd4d93125a6314076f4
0XSoumya <soumyasahoo.2907@gmail.com>
2025-04-27 21:42:10 +0530

    code for sequential chains in langchain

---
+++ chains/Sequential_chain.py
@@ -0,0 +1,25 @@
+from langchain_mistralai import ChatMistralAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+first_prompt = PromptTemplate(
+    template = 'generate a detailed report about {topic}',
+    input_variables = ['topic']
+)
+
+second_prompt = PromptTemplate(
+    template='summarize the following text {text}',
+    input_variables=['text']
+)
+
+model = ChatMistralAI(model = 'mistral-small')
+
+parser = StrOutputParser()
+
+chain = first_prompt | model | parser | second_prompt | model | parser
+
+result = chain.invoke({'topic' : 'global warming'})
+print(result)
