18afa3a4b9befe74ddd3f0c97b64bb38fa1fcbe7
0XSoumya <soumyasahoo.2907@gmail.com>
2025-04-27 12:04:38 +0530

    Visual Upgrade

--- Prompts/terminal_chatbot.py
+++ Prompts/terminal_chatbot.py
@@ -2,22 +2,35 @@ from langchain_mistralai import ChatMistralAI
 from dotenv import load_dotenv
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from colorama import init, Fore, Style
+from pyfiglet import Figlet
+
 
 load_dotenv()
 
+init(autoreset=True)  # For colored text on Windows/Linux
+
 model = ChatMistralAI(model = 'mistral-small')
+
+# following code is only for aesthetics
+print(Fore.YELLOW + "-----------------------------------------------\n")
+figlet = Figlet(font='slant')  # you can change font styles if you want
+ascii_header = figlet.renderText('Mistral Chat')
+print(Fore.CYAN + Style.BRIGHT + ascii_header)
+print(Fore.CYAN + "💬 Welcome to Mistral Chat! (type 'exit' to leave) 💬")
+print(Fore.YELLOW + "-----------------------------------------------\n")
 
 chat_history=[
     SystemMessage(content='you are a helpful assistant who gives concise answers to the queries')
 
 ]
 
+
 while True:
-    query = input('You :')
+    query = input(Fore.GREEN + '👤You :' + Style.RESET_ALL)
     chat_history.append(HumanMessage(content= query))
-    if query == 'exit' :
-        
+    if query.lower() == 'exit' :
+        print(Fore.RED + "\n 🤖 Mistral :Goodbye! 👋")
         break
     response = model.invoke(chat_history)
     chat_history.append(AIMessage(content=response.content))
-    print('Mistral :', response.content)
+    print(Fore.BLUE + Style.BRIGHT + '🤖 Mistral: ' + Style.RESET_ALL + response.content)
