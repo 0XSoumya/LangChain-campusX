d737a5bcb8b0ccf85ede14c3c843759a9677378f
0XSoumya <soumyasahoo.2907@gmail.com>
2025-04-27 21:25:49 +0530

    added code for simple chains

---
+++ chains/Simple_chain.py
@@ -0,0 +1,24 @@
+from langchain_mistralai import ChatMistralAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+load_dotenv()
+
+# creating prompt:
+prompt = PromptTemplate(
+    template = 'generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatMistralAI(model = 'mistral-small')
+parser = StrOutputParser()
+
+chain = prompt | model | parser # creating a simple chain
+
+result = chain.invoke({'topic': 'baking'})
+
+print(result)
+            
+chain.get_graph().print_ascii
