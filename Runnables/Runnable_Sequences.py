20d54e19b038bcbf8c1d4e7ffa2e5503f5be121d
0XSoumya <soumyasahoo.2907@gmail.com>
2025-05-03 14:51:57 +0530

    added code

---
+++ Runnables/Runnable_Sequences.py
@@ -0,0 +1,21 @@
+# simple code to demonstrate runnable sequences
+
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template = 'write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatMistralAI(model = 'mistral-small')
+parser = StrOutputParser()
+
+chain = RunnableSequence(prompt,model,parser)
+
+print(chain.invoke({'topic':'AI'}))
